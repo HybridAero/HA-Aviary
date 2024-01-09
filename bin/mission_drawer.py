20c34fe9b1c0e324b43999d426b02a1e38fb4189
Mathonwy Thomas <mathonwy.thomas@hybridaero.co.uk>
2024-01-09 11:05:57 +0000

    Add time range bars to plots

--- bin/mission_drawer.py
+++ bin/mission_drawer.py
@@ -17,6 +17,8 @@ class MissionProfile:
             # vertical_spacing=0.1,
         )
         time_nominal = []
+        time_errors_lwr = []
+        time_errors_upr = []
         mach_nominal = []
         alt_nominal = []
         mach_box = []
@@ -31,16 +33,28 @@ class MissionProfile:
             m_min, m_max = phase_data["mach_bounds"][0]
             a_min, a_max = phase_data["altitude_bounds"][0]
             time_nominal.append(t_init_guess)
+            time_errors_lwr.append(t_init_guess - phase_data["initial_bounds"][0][0])
+            time_errors_upr.append(phase_data["initial_bounds"][0][1] - t_init_guess)
             mach_nominal.append(phase_data["initial_mach"][0])
             alt_nominal.append(phase_data["initial_altitude"][0])
             mach_box.append((t_init_guess, t_init_guess + duration_init_guess, m_min, m_max))
             alt_box.append((t_init_guess, t_init_guess + duration_init_guess, a_min, a_max))
         # Need to add the final ones too
         time_nominal.append(t_init_guess + duration_init_guess)
+        time_errors_lwr.append(
+            time_nominal[-1]
+            - (phase_data["initial_bounds"][0][0] + phase_data["duration_bounds"][0][0])
+        )
+        time_errors_upr.append(
+            (phase_data["initial_bounds"][0][1] + phase_data["duration_bounds"][0][1])
+            - time_nominal[-1]
+        )
+
         mach_nominal.append(phase_data["final_mach"][0])
         alt_nominal.append(phase_data["final_altitude"][0])
 
         # Draw shaded boxes of the available region (draw these first so they're 'behind' the traces)
+        range_boxes_colour = "rgba(50, 50, 255, 0.25)"
         show_mach_legend = True
         for box in mach_box:
             # bl, br, tr, tl
@@ -51,7 +65,7 @@ class MissionProfile:
                     x=xs,
                     y=ys,
                     fill="toself",
-                    fillcolor="rgba(50, 255, 0, 0.4)",
+                    fillcolor=range_boxes_colour,
                     mode="none",
                     name="Permissible Mach Range",
                     legendgroup="machrange",
@@ -71,7 +85,7 @@ class MissionProfile:
                     x=xs,
                     y=ys,
                     fill="toself",
-                    fillcolor="rgba(50, 255, 0, 0.4)",
+                    fillcolor=range_boxes_colour,
                     mode="none",
                     name="Permissible Altitude Range",
                     legendgroup="altrange",
@@ -81,6 +95,55 @@ class MissionProfile:
                 col=2,
             )
             show_alt_legend = False
+
+        #  Error bars for the X location (i.e. time range for each phase)
+        fig.add_trace(
+            go.Scatter(
+                x=time_nominal,
+                y=mach_nominal,
+                mode="lines",
+                line_shape="linear",
+                name="Time Range",
+                line={"color": "blue"},
+                opacity=0.5,
+                error_x=dict(
+                    type="data",
+                    symmetric=False,
+                    color="blue",
+                    array=time_errors_upr,
+                    arrayminus=time_errors_lwr,
+                ),
+                legendgroup="timeerror",
+                legendrank=20,
+                showlegend=True,
+            ),
+            row=1,
+            col=1,
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=time_nominal,
+                y=alt_nominal,
+                mode="lines+markers",
+                line_shape="linear",
+                name="Time Range",
+                line={"color": "blue"},
+                opacity=0.5,
+                error_x=dict(
+                    type="data",
+                    symmetric=False,
+                    color="blue",
+                    array=time_errors_upr,
+                    arrayminus=time_errors_lwr,
+                ),
+                legendgroup="timeerror",
+                showlegend=False,
+            ),
+            row=1,
+            col=2,
+        )
+
+        # Main traces
         fig.add_trace(
             go.Scatter(
                 x=time_nominal,
@@ -121,16 +184,9 @@ class MissionProfile:
         fig["layout"]["xaxis2"]["title"] = "Time (min)"
         fig["layout"]["yaxis"]["title"] = "Mach #"
         fig["layout"]["yaxis2"]["title"] = "Altitude (ft)"
+        fig.layout["yaxis"]["rangemode"] = "tozero"
+        fig.layout["yaxis2"]["rangemode"] = "tozero"
+        fig.update_layout(
+            title_text="Mission Profile",
+        )
         fig.show()
-
-
-"""
-
-
-from bin.ha_helper import get_phase_info
-from bin.mission_drawer import MissionProfile as MP
-mp = MP(get_phase_info())
-mp.plot()
-
-
-"""
